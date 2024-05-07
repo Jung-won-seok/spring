2d17559a28c9343e4452f139ac42083418c80517
Jung-won-seok <ukksj0621@naver.com>
2024-05-07 14:56:43 +0900

    Spring

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,7 +13,11 @@ import org.springframework.context.annotation.FilterType;
         basePackageClasses = AutoAppConfig.class, //자기패키지부터 찾는다.
 
         // Configuration 에노테이션 한 것은 컴포넌트 스캔 하지 않는다
-        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
